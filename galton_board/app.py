6af147c8e0665793d063f493eee9cc20e45fdffc
Alireza Alavi <alireza.alavi237@gmail.com>
2025-12-06 19:27:07 +0100

    fixed whitespace and indentations

--- galton_board/app.py
+++ galton_board/app.py
@@ -11,159 +11,222 @@ BG_COLOR = '#202020'
 BOX_COLOR = (200, 200, 200)
 
 class Ball:
-	def __init__(self, space: pm.Space, *, pos, mass, color, radius = None, btype = pm.Body.DYNAMIC) -> None:
-		radius = radius or mass
-		moment = pm.moment_for_circle(mass=mass, inner_radius=0, outer_radius=radius)
-		self.body = pm.Body(mass=mass, moment=moment, body_type=btype)
-		self.body.position = pos
-		self.shape = pm.Circle(self.body, radius=radius)
-		self.shape.color = color
+    def __init__(
+        self, space: pm.Space, *, pos, mass, color, radius=None, btype=pm.Body.DYNAMIC
+    ) -> None:
+        radius = radius or mass
+        moment = pm.moment_for_circle(mass=mass, inner_radius=0, outer_radius=radius)
+        self.body = pm.Body(mass=mass, moment=moment, body_type=btype)
+        self.body.position = pos
+        self.shape = pm.Circle(self.body, radius=radius)
+        self.shape.color = color
 
-		self.shape.density = 3
-		self.shape.elasticity = 0.3
-		self.shape.friction = 0.6
+        self.shape.density = 4
+        self.shape.elasticity = 0.35
+        self.shape.friction = 0.9
 
-		space.add(self.body, self.shape)
+        space.add(self.body, self.shape)
 
-
-	def draw(self, screen: pg.surface) -> None:
-		self.pg = pg.draw.circle(screen, self.shape.color, center=self.body.position, radius=self.shape.radius)
-
+    def draw(self, screen: pg.surface) -> None:
+        self.pg = pg.draw.circle(
+            screen,
+            self.shape.color,
+            center=self.body.position,
+            radius=self.shape.radius,
+        )
 
 
 class Segment:
-	def __init__(self, space: pm.Space, *, pos_a, pos_b, mass = None, color, thickness = None, btype = pm.Body.STATIC) -> None:
-		
-		self.body = pm.Body(mass=mass, body_type=btype)
-		self.shape = pm.Segment(self.body, a=pos_a, b=pos_b, radius=thickness)
-		self.shape.color = color
-		self.shape.elasticity = 0.4
-		self.shape.friction = 0.1
+    def __init__(
+        self,
+        space: pm.Space,
+        *,
+        pos_a,
+        pos_b,
+        mass=None,
+        color,
+        thickness=None,
+        btype=pm.Body.STATIC,
+    ) -> None:
+        self.body = pm.Body(mass=mass, body_type=btype)
+        self.shape = pm.Segment(self.body, a=pos_a, b=pos_b, radius=thickness)
+        self.shape.color = color
+        self.shape.elasticity = 0.4
+        self.shape.friction = 0.1
 
-		space.add(self.body, self.shape)
+        space.add(self.body, self.shape)
 
+    def calculate_polygon_points(self) -> tuple[tuple, tuple, tuple, tuple]:
+        start = self.shape.a
+        end = self.shape.b
 
-	def calculate_polygon_points(self) -> tuple[tuple, tuple, tuple, tuple]:
-		start = self.shape.a
-		end = self.shape.b
-		
-		# Compute the direction vector and its length
-		dx = end[0] - start[0]
-		dy = end[1] - start[1]
-		length = math.hypot(dx, dy)
-	
-		# Normalize the direction vector
-		dx /= length
-		dy /= length
-		
-		# Compute perpendicular vector
-		perp_dx = -dy
-		perp_dy = dx
-		
-		# Compute the four corners of the polygon
-		offset = self.shape.radius
-		p1 = (start[0] + offset * perp_dx, start[1])
-		p2 = (start[0] - offset * perp_dx, start[1] - offset * perp_dy)
-		p3 = (end[0] - offset * perp_dx, end[1] - offset * perp_dy)
-		p4 = (end[0] + offset * perp_dx, end[1] )
+        # Compute the direction vector and its length
+        dx = end[0] - start[0]
+        dy = end[1] - start[1]
+        length = math.hypot(dx, dy)
 
-		return (p1, p2, p3, p4)
+        # Normalize the direction vector
+        dx /= length
+        dy /= length
 
+        # Compute perpendicular vector
+        perp_dx = -dy
+        perp_dy = dx
 
-	def draw(self, screen: pg.surface) -> None:
-		self.pg = pg.draw.polygon(screen, color=self.shape.color, points=self.calculate_polygon_points())
+        # Compute the four corners of the polygon
+        offset = self.shape.radius
+        p1 = (start[0] + offset * perp_dx, start[1])
+        p2 = (start[0] - offset * perp_dx, start[1] - offset * perp_dy)
+        p3 = (end[0] - offset * perp_dx, end[1] - offset * perp_dy)
+        p4 = (end[0] + offset * perp_dx, end[1])
+
+        return (p1, p2, p3, p4)
+
+    def draw(self, screen: pg.surface) -> None:
+        self.pg = pg.draw.polygon(
+            screen, color=self.shape.color, points=self.calculate_polygon_points()
+        )
 
 
 class Simulation:
-	def __init__(self) -> None:
-		# pymunk settings
-		self.space = pm.Space()
-		self.space.gravity = (0, 1000)
-		self.space.damping = 0.93
-		
-		# pygame settings
-		self.screen = pg.display.set_mode((WIDTH, HEIGHT))
-		pg.display.set_caption('Pendulum Simulation')
-		self.clock = pg.time.Clock()
-		self.fps = FPS
+    def __init__(self) -> None:
+        # pymunk settings
+        self.space = pm.Space()
+        self.space.gravity = (0, 1000)
+        self.space.damping = 0.93
+
+        # pygame settings
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption("Pendulum Simulation")
+        self.clock = pg.time.Clock()
+        self.fps = FPS
+
+        self.funnel = [
+            Segment(
+                self.space, pos_a=(0, 0), pos_b=(0, 120), color=BOX_COLOR, thickness=5
+            ),
+            Segment(
+                self.space,
+                pos_a=(0, 120),
+                pos_b=(WIDTH / 2 - 30, 200),
+                color=BOX_COLOR,
+                thickness=5,
+            ),
+            Segment(
+                self.space,
+                pos_a=(WIDTH / 2 - 30, 200),
+                pos_b=(WIDTH / 2 - 30, 260),
+                color=BOX_COLOR,
+                thickness=2.5,
+            ),
+            Segment(
+                self.space,
+                pos_a=(WIDTH, 0),
+                pos_b=(WIDTH, 120),
+                color=BOX_COLOR,
+                thickness=5,
+            ),
+            Segment(
+                self.space,
+                pos_a=(WIDTH, 120),
+                pos_b=(WIDTH / 2 + 30, 200),
+                color=BOX_COLOR,
+                thickness=5,
+            ),
+            Segment(
+                self.space,
+                pos_a=(WIDTH / 2 + 30, 200),
+                pos_b=(WIDTH / 2 + 30, 260),
+                color=BOX_COLOR,
+                thickness=2.5,
+            ),
+        ]
+        NUM_BARS = 17
+        self.bars = [
+            Segment(
+                self.space,
+                pos_a=(0, HEIGHT),
+                pos_b=(WIDTH, HEIGHT),
+                color=BOX_COLOR,
+                thickness=4,
+            ),
+        ]
+
+        for i in range(NUM_BARS + 1):
+            self.bars.append(
+                Segment(
+                    self.space,
+                    pos_a=(i * WIDTH / NUM_BARS, HEIGHT),
+                    pos_b=(i * WIDTH / NUM_BARS, HEIGHT - 350),
+                    color=BOX_COLOR,
+                    thickness=2,
+                ),
+            )
+
+        NUM_PEGS_ROWS = 10
+        DPEGS = WIDTH // 16
+        self.pegs = [
+            [
+                Ball(
+                    self.space,
+                    pos=(i, r * 30 + 310),
+                    mass=10,
+                    color=BOX_COLOR,
+                    btype=pm.Body.STATIC,
+                )
+                for i in range(DPEGS // 2 if r % 2 else DPEGS, WIDTH, DPEGS)
+            ]
+            for r in range(NUM_PEGS_ROWS)
+        ]
+        NUM_BALLS = 720
+        self.balls = [
+            Ball(
+                self.space,
+                pos=(n * WIDTH // NUM_BALLS - 45, n // 6),
+                color=(200, 170, 0),
+                mass=5,
+            )
+            for n in range(1, NUM_BALLS + 1)
+        ]
+
+    def draw(self) -> None:
+        for seg in self.funnel:
+            seg.draw(self.screen)
+        for bar in self.bars:
+            bar.draw(self.screen)
+
+        for pegs in self.pegs:
+            for peg in pegs:
+                peg.draw(self.screen)
+
+        for ball in self.balls:
+            ball.draw(self.screen)
+
+    def step(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                sys.exit()
+            if event.type == pg.MOUSEBUTTONDOWN:
+                pos = event.pos
+                self.balls.extend(
+                    [
+                        Ball(self.space, pos=pos, color=(200, 170, 0), mass=20)
+                        for _ in range(50)
+                    ]
+                )
+
+        self.screen.fill(BG_COLOR)
+
+        self.draw()
+
+        pg.display.update()
+        self.clock.tick(self.fps)
+        self.space.step(1 / self.fps)
 
 
-		self.funnel = [
-			Segment(self.space, pos_a=(0, 0), pos_b=(0, 120), color=BOX_COLOR, thickness=5),
-			Segment(self.space, pos_a=(0, 120), pos_b=(WIDTH/2 - 30, 200), color=BOX_COLOR, thickness=5),
-			Segment(self.space, pos_a=(WIDTH/2 - 30, 200), pos_b=(WIDTH/2 - 30, 260), color=BOX_COLOR, thickness=2.5),
+if __name__ == "__main__":
+    sim = Simulation()
 
-			Segment(self.space, pos_a=(WIDTH, 0), pos_b=(WIDTH, 120), color=BOX_COLOR, thickness=5),
-			Segment(self.space, pos_a=(WIDTH, 120), pos_b=(WIDTH/2 + 30, 200), color=BOX_COLOR, thickness=5),
-			Segment(self.space, pos_a=(WIDTH/2+30, 200), pos_b=(WIDTH/2 + 30, 260), color=BOX_COLOR, thickness=2.5),
-		]
-		NUM_BARS = 17
-		self.bars = [
-			Segment(self.space, pos_a=(0, HEIGHT), pos_b=(WIDTH, HEIGHT), color=BOX_COLOR, thickness=4),
-			
-		]
-
-		for i in range(NUM_BARS+1):
-			self.bars.append(
-				Segment(
-					self.space, 
-					pos_a=(i*WIDTH/NUM_BARS, HEIGHT), 
-					pos_b=(i*WIDTH/NUM_BARS, HEIGHT - 350), 
-					color=BOX_COLOR, 
-					thickness=2
-				),
-			)
-
-		NUM_PEGS_ROWS = 10
-		DPEGS = WIDTH//16
-		self.pegs = [
-			[
-				Ball(self.space, pos=(i, r*30 + 310), mass=10, color=BOX_COLOR, btype=pm.Body.STATIC)
-				for i in range(DPEGS//2 if r%2 else DPEGS, WIDTH, DPEGS)
-			]
-			for r in range(NUM_PEGS_ROWS)
-		]
-		NUM_BALLS = 720
-		self.balls = [
-			Ball(self.space, pos=(n * WIDTH//NUM_BALLS-45, n//6), color=(200, 170, 0), mass=5)
-			for n in range(1, NUM_BALLS+1)
-		]
-
-	def draw(self) -> None:
-		for seg in self.funnel:
-			seg.draw(self.screen)
-		for bar in self.bars:
-			bar.draw(self.screen)
-
-		for pegs in self.pegs:
-			for peg in pegs:
-				peg.draw(self.screen)
-
-		for ball in self.balls:
-			ball.draw(self.screen)
-
-	def step(self):
-		for event in pg.event.get():
-			if event.type == pg.QUIT:
-				pg.quit()
-				sys.exit()
-			if event.type == pg.MOUSEBUTTONDOWN:
-				pos = event.pos
-				self.balls.extend([Ball(self.space, pos=pos, color=(200, 170, 0), mass=5) for _ in range(50)])
-	
-			
-		self.screen.fill(BG_COLOR)
-		
-		self.draw()
-		
-		pg.display.update()
-		self.clock.tick(self.fps)
-		self.space.step(1/self.fps)
-
-
-
-if __name__ == '__main__':
-	sim = Simulation()
-	
-	while True:
-		sim.step()
+    while True:
+        sim.step()
--- pendulum/app.py
+++ pendulum/app.py
@@ -15,122 +15,126 @@ BALL_COLOR = (220, 180, 0, 255)
 
 
 class Ball:
-	"""
-	Creates a ball, and holds many attributes like body, shape and mass and other physical properties.
-	And also graphical properties like size(radius) and color
-	"""
-	def __init__(
-			self, 
-			space: pm.Space, 
-			pos: tuple[int, int],
-			color: str | tuple[int, int, int] | pg.Color,
-			mass: int = MASS,
-			body_type = pm.Body.DYNAMIC
-	) -> None:
+    """
+    Creates a ball, and holds many attributes like body, shape and mass and other physical properties.
+    And also graphical properties like size(radius) and color
+    """
 
-		self.body = pm.Body(mass=mass, body_type=body_type)
-		self.body.position = pos
-		
-		radius = mass / 2
-		self.body.moment = pm.moment_for_circle(mass, inner_radius=0, outer_radius=radius)
-		self.shape = pm.Circle(self.body, radius=radius)
-		self.shape.color = color
+    def __init__(
+        self,
+        space: pm.Space,
+        pos: tuple[int, int],
+        color: str | tuple[int, int, int] | pg.Color,
+        mass: int = MASS,
+        body_type=pm.Body.DYNAMIC,
+    ) -> None:
+        self.body = pm.Body(mass=mass, body_type=body_type)
+        self.body.position = pos
 
-		self.shape.density = 1
-		self.shape.elasticity = 0.9
-		
-		space.add(self.body, self.shape)
+        radius = mass / 2
+        self.body.moment = pm.moment_for_circle(
+            mass, inner_radius=0, outer_radius=radius
+        )
+        self.shape = pm.Circle(self.body, radius=radius)
+        self.shape.color = color
+
+        self.shape.density = 1
+        self.shape.elasticity = 0.9
+
+        space.add(self.body, self.shape)
 
 
 class Rod:
-	"""
-	Creates a rod, connecting two given bodies(or a given body and a coordinate in space)
-	Using a PinJoint
-	"""
-	def __init__(
-			self, 
-			space: pm.Space, 
-			body1: pm.Body,
-			body2: pm.Body | tuple[int, int], # a body or a coordinate
-	) -> None:
-		self.body1 = body1
+    """
+    Creates a rod, connecting two given bodies(or a given body and a coordinate in space)
+    Using a PinJoint
+    """
 
-		
-		if isinstance(body2, pm.Body):
-			self.body2 = body2
-		else:
-			self.body2 = space.static_body
-			# using body2 as the position
-			self.body2.position = body2
+    def __init__(
+        self,
+        space: pm.Space,
+        body1: pm.Body,
+        body2: pm.Body | tuple[int, int],  # a body or a coordinate
+    ) -> None:
+        self.body1 = body1
 
-		self.joint = pm.PinJoint(self.body1, self.body2)
+        if isinstance(body2, pm.Body):
+            self.body2 = body2
+        else:
+            self.body2 = space.static_body
+            # using body2 as the position
+            self.body2.position = body2
 
-		space.add(self.joint)
+        self.joint = pm.PinJoint(self.body1, self.body2)
 
-	
+        space.add(self.joint)
+
+
 class Pendulum:
-	def __init__(
-			self, 
-			space: pm.Space, 
-			*, 
-			piv_pos: tuple,
-			length: int,
-			theta: float,
-			ball_color: tuple[int, int, int, int] 
-	) -> None:
-		"""
-		Creates pendulum, based on length and angle theta(degrees), ball color and pivot point coordinate
-		piv_pos, is based on positive y-axis going down
-		"""
-		piv_pos = piv_pos[0], HEIGHT - piv_pos[1]
-		theta = theta / 180 * 3.1415
-		ball_pos = piv_pos[0] -  length * sin(theta), piv_pos[1] -  length * cos(theta)
+    def __init__(
+        self,
+        space: pm.Space,
+        *,
+        piv_pos: tuple,
+        length: int,
+        theta: float,
+        ball_color: tuple[int, int, int, int],
+    ) -> None:
+        """
+        Creates pendulum, based on length and angle theta(degrees), ball color and pivot point coordinate
+        piv_pos, is based on positive y-axis going down
+        """
+        piv_pos = piv_pos[0], HEIGHT - piv_pos[1]
+        theta = theta / 180 * 3.1415
+        ball_pos = piv_pos[0] - length * sin(theta), piv_pos[1] - length * cos(theta)
 
-
-		self.ball = Ball(space, pos=ball_pos, color=ball_color)
-		self.rod = Rod(space, body1=self.ball.body, body2=piv_pos)
+        self.ball = Ball(space, pos=ball_pos, color=ball_color)
+        self.rod = Rod(space, body1=self.ball.body, body2=piv_pos)
 
 
 class Simulation:
-	def __init__(self) -> None:
-		# pymunk settings
-		self.space = pm.Space()
-		self.space.gravity = (0, -1500)
-		self.space.damping = 0.93
-		
-		# pygame settings
-		self.screen = pg.display.set_mode((WIDTH, HEIGHT))
-		pg.display.set_caption('Pendulum Simulation')
-		self.clock = pg.time.Clock()
-		self.fps = FPS
+    def __init__(self) -> None:
+        # pymunk settings
+        self.space = pm.Space()
+        self.space.gravity = (0, -1500)
+        self.space.damping = 0.93
 
-		# pymunk and pygame integration
-		self.options = pgutil.DrawOptions(self.screen)
-		pgutil.positive_y_is_up = True
+        # pygame settings
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption("Pendulum Simulation")
+        self.clock = pg.time.Clock()
+        self.fps = FPS
 
-		rod_color = (255, 255, 255, 255)
-		self.options.constraint_color = rod_color
-		
+        # pymunk and pygame integration
+        self.options = pgutil.DrawOptions(self.screen)
+        pgutil.positive_y_is_up = True
 
-	def step(self):
-		for event in pg.event.get():
-			if event.type == pg.QUIT:
-				pg.quit()
-				sys.exit()
-		
-		self.screen.fill(BG_COLOR)
-		
-		self.space.debug_draw(self.options)
-		pg.display.update()
-		self.clock.tick(self.fps)
-		self.space.step(1/self.fps)
+        rod_color = (255, 255, 255, 255)
+        self.options.constraint_color = rod_color
 
+    def step(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                sys.exit()
+
+        self.screen.fill(BG_COLOR)
+
+        self.space.debug_draw(self.options)
+        pg.display.update()
+        self.clock.tick(self.fps)
+        self.space.step(1 / self.fps)
 
 
-if __name__ == '__main__':
-	sim = Simulation()
-	Pendulum(sim.space, piv_pos=(1*WIDTH//2, 50), length=300, theta=30, ball_color=BALL_COLOR)
+if __name__ == "__main__":
+    sim = Simulation()
+    Pendulum(
+        sim.space,
+        piv_pos=(1 * WIDTH // 2, 50),
+        length=300,
+        theta=30,
+        ball_color=BALL_COLOR,
+    )
 
-	while True:
-		sim.step()
-		
+    while True:
+        sim.step()
